ab3478434b633143aecb8ce6addda8331a6934c5
anton <chadik.free@gmail.com>
2024-04-30 12:50:41 +0500

    content fix

--- src/data/petProjects.js
+++ src/data/petProjects.js
@@ -4,7 +4,7 @@ const petProjects = [
     {
         title: 'Скоро тут что-то будет',
         icon: mdiHeadQuestionOutline,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore consequatur quisquam natus officiis reiciendis cumque, ad, nesciunt impedit magnam labore provident aliquid repudiandae, porro exercitationem molestiae itaque recusandae autem optio.'
+        description: ''
         // links: {
         //     demo: 'https://www.google.com',
         //     source: 'https://www.google.com',
